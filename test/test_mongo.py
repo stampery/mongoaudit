6ce66ea9eb1fb27c4d93147f7fbcd56d6e433e23
Adán Sánchez de Pedro Crespo <adan@stampery.com>
2017-02-02 20:49:28 +0100

    Travis CI

--- test/test_mongo.py
+++ test/test_mongo.py
@@ -1,12 +1,14 @@
 import unittest
-from src.tools import *
-from src.testers import *
+import sys
+sys.path.insert(0, 'src')
+from tools import *
+from testers import *
 
 
 class MongoauditTest(unittest.TestCase):
 
     def create_type(self, **kwargs):
-            return type("mock", (object,), kwargs)
+        return type("mock", (object,), kwargs)
 
     def info_obj(self, info):
         return self.create_type(tester=self.create_type(info=info))
@@ -14,7 +16,6 @@ class MongoauditTest(unittest.TestCase):
     def ver_obj(self, ver):
         return self.info_obj({"version": ver})
 
-    
 
     def test_alert1(self):
         self.assertFalse(alerts_dec012015(self.info_obj({"version": "3.0.3", "modules":["enterprise"]})))
